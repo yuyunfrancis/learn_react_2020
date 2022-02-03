0d65e8af9c1f73785ebf4c0f430884a1d05e10a9
yuyunfrancis <yuyun.francis@ictuniversity.org>
2022-02-03 13:28:29 +0100

    useReducer done

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,13 +2,12 @@ import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
 // reducer function
-
-const reducer = (state, action) => {};
+import { reducer } from "./reducer";
 
 const defaultState = {
   people: [],
   isModalOpen: false,
-  modalContent: "hello world",
+  modalContent: "",
 };
 
 const Index = () => {
@@ -18,13 +17,23 @@ const Index = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_ITEM", payload: newItem });
+      setName("");
     } else {
+      dispatch({ type: "NO_VALUE" });
     }
+  };
+
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
   };
 
   return (
     <>
-      {state.isModalOpen && <Modal />}
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -37,8 +46,15 @@ const Index = () => {
       </form>
       {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className="item">
             <h4>{person.name}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
+            >
+              remove item
+            </button>
           </div>
         );
       })}
