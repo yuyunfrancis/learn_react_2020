5aba6bfa7906260f88dfb8d56b4e354ff98448c4
yuyunfrancis <yuyun.francis@ictuniversity.org>
2022-02-08 20:20:05 +0100

    prop drilling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
-import Setup from "./tutorial/6-useReducer/setup";
+import React from 'react';
+import Setup from './tutorial/7-prop-drilling/setup/1-prop-drilling';
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
       <Setup />
     </div>
   );
